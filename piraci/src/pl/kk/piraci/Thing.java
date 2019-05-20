2399c075cef206ac0af19efda3cd9d3048a632e4
Krzysztof Koziol <kkoziol@gmail.com>
2019-05-20 02:45:41 +0200

    Piraci - prototyp rozwiazanie

--- piraci/src/pl/kk/piraci/Thing.java
+++ piraci/src/pl/kk/piraci/Thing.java
@@ -1,6 +1,17 @@
 package pl.kk.piraci;
 
-public class Thing {
+public class Thing implements Cloneable {
 
+    @Override
+    public Thing clone() {
+        Thing clone = null;
+        try {
+            clone = (Thing) super.clone();
+            // here make deep copy for mutable objects
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
 
 }
