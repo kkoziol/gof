185f72051db02b7e65196af463cf516c4802e08c
Krzysztof Kozioł <Krzysztof.Koziol@fisglobal.com>
2019-05-21 10:20:11 +0200

    Poprawka

--- piraci/src/main/java/pl/kk/piraci/ChineseFactory.java
+++ piraci/src/main/java/pl/kk/piraci/ChineseFactory.java
@@ -31,10 +31,12 @@ public class ChineseFactory {
         toClone = toy;
     }
 
-    public Thing produceNextItem() {
-        return new Toy(toClone.getNazwa());
+    public Thing produceNextCar() {
+        return new RaceCar();
     }
-
+    public Thing produceNextBear() {
+        return new Bear();
+    }
     public Thing cloneNextItem() {
         return toClone.clone();
     }
@@ -47,26 +49,26 @@ public class ChineseFactory {
         factory.setProductionLine(new RaceCar());
         long start = System.nanoTime();
         for (int i = 1; i <= 10; i++)
-            factory.produceNextItem();
+            factory.produceNextCar();
         long duration = System.nanoTime() - start;
-        System.out.println("Produkcja  10 RaceCar:" + duration + " srednio na jedą:" + duration / 10);
+        System.out.println("Produkcja  10 RaceCar:" + duration + " srednio na jedną:" + duration / 10);
         start = System.nanoTime();
         for (int i = 1; i <= 10; i++)
             factory.cloneNextItem();
         duration = System.nanoTime() - start;
-        System.out.println("Klonowanie 10 RaceCar:" + duration + " srednio na jedą:" + duration / 10);
+        System.out.println("Klonowanie 10 RaceCar:" + duration + " srednio na jedną:" + duration / 10);
 
         factory.setProductionLine(new Bear());
         start = System.nanoTime();
         for (int i = 1; i <= 10; i++)
-            factory.produceNextItem();
+            factory.produceNextCar();
         duration = System.nanoTime() - start;
-        System.out.println("Produkcja  10 Misiów:" + duration + " srednio na jedą:" + duration / 10);
+        System.out.println("Produkcja  10 Misiów:" + duration + " srednio na jedną:" + duration / 10);
         start = System.nanoTime();
         for (int i = 1; i <= 10; i++)
             factory.cloneNextItem();
         duration = System.nanoTime() - start;
-        System.out.println("Klonowanie 10 Misiów:" + duration + " srednio na jedą:" + duration / 10);
+        System.out.println("Klonowanie 10 Misiów:" + duration + " srednio na jedną:" + duration / 10);
 
     }
 }
