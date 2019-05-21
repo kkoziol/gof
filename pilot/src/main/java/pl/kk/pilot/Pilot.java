625b88380b0cb4f2794f29499ee8a162abee7176
Krzysztof Kozioł <Krzysztof.Koziol@fisglobal.com>
2019-05-21 13:08:28 +0200

    pilot - menu przelaczanie

--- pilot/src/main/java/pl/kk/pilot/Pilot.java
+++ pilot/src/main/java/pl/kk/pilot/Pilot.java
@@ -1,87 +1,67 @@
 package pl.kk.pilot;
 
-
-import pl.kk.pilot.comands.Command;
-import pl.kk.pilot.comands.CommandMakro;
-import pl.kk.pilot.comands.CommandPrzelaczMenu;
-import pl.kk.pilot.comands.CommandZakoncz;
-import pl.kk.pilot.comands.garaz.CommandGarazOtworzDrzwi;
-import pl.kk.pilot.comands.garaz.CommandGarazZamknijDrzwi;
-import pl.kk.pilot.comands.swiatlo.CommandSwiatloWlacz;
-import pl.kk.pilot.comands.swiatlo.CommandSwiatloWylacz;
-import pl.kk.pilot.comands.wentylator.*;
-import pl.kk.pilot.garaz.Garaz;
-import pl.kk.pilot.swiatlo.Swiatlo;
-import pl.kk.pilot.tv.Telewizor;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.TreeMap;
+
+import pl.kk.pilot.comands.Command;
+import pl.kk.pilot.comands.menu.MenuDom;
+import pl.kk.pilot.comands.menu.MenuPilota;
+import pl.kk.pilot.comands.menu.MenuTv;
 
 public class Pilot {
 
-    private Map<String, Command> przyciski = new TreeMap<String, Command>();
+	public final MenuPilota MENU_DOMOWE = new MenuDom(this);
+	public final MenuPilota MENU_TV = new MenuTv(this);
+	MenuPilota stan;
 
-    public Pilot() {
-        System.out.println("-----> Rozpoczęcie pracy pilota <-----");
-        przyciski.put("1", new CommandSwiatloWlacz());
-        przyciski.put("2", new CommandSwiatloWylacz());
-        przyciski.put("3", new CommandGarazOtworzDrzwi());
-        przyciski.put("4", new CommandGarazZamknijDrzwi());
-        przyciski.put("5", new CommandWentylatorWlacz());
-        przyciski.put("6", new CommandWentylatorObroty1());
-        przyciski.put("7", new CommandWentylatorObroty2());
-        przyciski.put("8", new CommandWentylatorObroty3());
-        przyciski.put("9", new CommandWentylatorWylacz());
-        przyciski.put("0", new CommandMakro(przyciski.get("1"), przyciski.get("5")));
-        przyciski.put("Z", new CommandZakoncz());
+	public Pilot() {
+		System.out.println("-----> Rozpoczęcie pracy pilota");
+		stan = MENU_DOMOWE;
+	}
 
+	public void kliknijPrzyciskNr(String numer) {
+		Command command = stan.getCommandForKey(numer.toUpperCase());
+		System.out.println("Kliknięto: " + numer);
+		command.execute();
+	}
 
-    }
+	private static boolean czyWcisnietoZnakWyjscia(String znak) {
+		return !(znak.toUpperCase().equals("Z"));
+	}
 
-    public void kliknijPrzyciskNr(String numer) {
-        if (przyciski.get(numer) != null)
-            przyciski.get(numer).execute();
-    }
+	public static void main(String[] args) {
+		Pilot pilot = new Pilot();
+		boolean czyDalej = true;
+		String wybranyNr = null;
 
-    private static boolean czyWcisnietoZnakWyjscia(String znak) {
-        return !(znak.toUpperCase().equals("Z"));
-    }
+		do {
+			System.out.println("");
 
-    public static void main(String[] args) {
-        Pilot pilot = new Pilot();
-        boolean czyDalej = true;
-        String wybranyNr = null;
+			pilot.wyswietlMenu();
+			System.out.print("Wybierz opcję: ");
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+				wybranyNr = br.readLine();
+				System.out.println("");
+				pilot.kliknijPrzyciskNr(wybranyNr);
+				czyDalej = czyWcisnietoZnakWyjscia(wybranyNr);
+			} catch (Exception e) {
+				System.out.println("");
+				System.out.println("Błędny wybór, spróbuj ponownie");
+			}
 
-        do {
-            System.out.println("");
+		} while (czyDalej);
+	}
 
-            pilot.wyswietlMenu();
-            System.out.println("-----> Wcisnij 'z' aby zakończyć <-----");
-            System.out.print("Wybierz opcję: ");
-            try {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-                wybranyNr = br.readLine();
-                System.out.println("");
-                pilot.kliknijPrzyciskNr(wybranyNr);
-                czyDalej = czyWcisnietoZnakWyjscia(wybranyNr);
-            } catch (Exception e) {
-                System.out.println("");
-                System.out.println("Błędny wybór, spróbuj ponownie");
-            }
-        } while (czyDalej);
-    }
+	private void wyswietlMenu() {
+		stan.wyswietlMenu();
+	}
 
-    private void wyswietlMenu() {
-        System.out.println("------>Menu<------");
-        for (Map.Entry<String, Command> entry : przyciski.entrySet()) {
-            System.out.println(entry.getKey() + "-" + entry.getValue().getOpis());
-        }
-
-    }
-
-    public void przelaczMenu() {
-
-    }
+	public void przelaczMenu() {
+		if (stan == MENU_DOMOWE) {
+			stan = MENU_TV;
+		} else {
+			stan = MENU_DOMOWE;
+		}
+	}
 }
