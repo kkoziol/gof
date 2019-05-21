130e172ca0c12cbfdd0027e5d0a70d9b43aab431
Krzysztof Kozioł <Krzysztof.Koziol@fisglobal.com>
2019-05-21 12:10:17 +0200

    pilot - stan - garaz

---
+++ pilot/src/main/java/pl/kk/pilot/garaz/GarazZamkniety.java
@@ -0,0 +1,15 @@
+package pl.kk.pilot.garaz;
+
+public class GarazZamkniety implements GarazStan {
+    
+	@Override
+	public void open() {
+		System.out.println("Otwieram garaz");
+		
+	}
+
+	@Override
+	public void close() {
+		System.out.println("Nic nie robię");
+	}
+}
